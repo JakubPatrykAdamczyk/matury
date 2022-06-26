c8093914922a2ff7fe74695dac42b80aa53520de
Jakub <jakubpatrykadamczyk@gmail.com>
2022-06-26 19:02:04 +0200

    a

--- 2022 maj/zad5/zad5.py
+++ 2022 maj/zad5/zad5.py
@@ -1,4 +1,5 @@
 from itertools import count
+import plotly.express as px
 import os
 
 mypath=os.getcwd()
@@ -40,4 +41,53 @@ for i in f:
 # print("Przemysl: " + str(city.count("Przemysl")))
 
 
-#5.2
+# #5.2
+# a=50
+# l=0
+# ln=0
+# st_dt=""
+# st_dtn=""
+# ed_dt=""
+# ed_dtn=""
+# for i in range(len(city)):
+#     if "Ogrodzieniec"==city[i]:
+#         if (a+1)==int(dat[i][0:2]):
+#             # print(dat[i][0:2])
+#             ln+=1
+#             ed_dtn=dat[i]
+#         else:
+#             ln=0
+#             st_dtn=dat[i]
+#         a=int(dat[i][0:2])
+#         if ln>l:
+#             l=ln
+#             st_dt=st_dtn
+#             ed_dt=ed_dtn
+# print (l)
+# print("od "+st_dt+" do "+ed_dt)
+
+# #5.3
+# gn=0
+# ma=0
+# ogr=0
+# prz=0
+# for i in range (len(city)):
+#     if "Gniezno"==city[i]: gn+=int(big[i])
+#     if "Malbork"==city[i]: ma+=int(big[i])
+#     if "Ogrodzieniec"==city[i]: ogr+=int(big[i])
+#     if "Przemysl"==city[i]: prz+=int(big[i])
+
+# print("Gniezno: " + str(gn))
+# print("Malbork: " + str(ma))
+# print("Ogrodzieniec: " + str(ogr))
+# print("Przemysl: " + str(prz))
+
+
+ 
+# labels = ['Gniezno','Malbork','Ogrodzieniec','Przemysl']
+# values = [gn, ma, ogr, prz]
+ 
+# fig = px.pie(values=values, names=labels,color_discrete_sequence=px.colors.sequential.RdBu)
+ 
+# fig.show()
+        
