a0543ed6f88889f7ac2207cab634000c18f6307c
Jakub <jakubpatrykadamczyk@gmail.com>
2022-06-27 15:47:02 +0200

    a

--- 2021 maj/zad5/tempCodeRunnerFile.py
+++ 2021 maj/zad5/tempCodeRunnerFile.py
@@ -1,2 +1,17 @@
-for i in range(10):
-#     print(list(mz.items())[len(mz)-i-1])
+
+# 5.3
+dziel={}
+print(wt[1][7:10])
+for i in range(1,len(wt)):
+    dziel[wt[i][7:10]]=0
+for i in range(1,len(wt)):
+    t=[]
+    for j in range(12):
+        n=0
+        n+=int(l[wt[i]][j])
+        n+=dziel[wt[i][7:10]]
+        t.append(n)
+    
+    dziel[wt[i][7:10]]=t
+    del t
+print(dziel)
--- 2021 maj/zad5/zad5.py
+++ 2021 maj/zad5/zad5.py
@@ -1,4 +1,5 @@
 import os
+import matplotlib.pyplot as plt
 
 mypath=os.getcwd()
 mypath+="\\matury\\"
@@ -43,15 +44,79 @@ for i in f:
 
 # for i in range(10):
 #     print(ans[i])
-# 5.2
-dziel={}
-print(wt[1][7:10])
-for i in range(1,len(wt)):
-    dziel[wt[i][7:10]]=0
-for i in range(1,len(wt)):
+
+# # 5.2
+# dziel={}
+# print(wt[1][7:10])
+# for i in range(1,len(wt)):
+#     dziel[wt[i][7:10]]=0
+# for i in range(1,len(wt)):
+#     n=0
+#     for j in range(12):
+#         n+=int(l[wt[i]][j])
+#     n+=dziel[wt[i][7:10]]
+#     dziel[wt[i][7:10]]=n
+# print(dziel)
+
+# # 5.3
+# dziel={}
+# # print(wt[1][7:10])
+# for i in range(1,len(wt)):
+#     dziel[wt[i][7:10]]=[0,0,0,0,0,0,0,0,0,0,0,0]
+# for i in range(1,len(wt)):
+#     t=[]
+#     for j in range(12):
+#         n=0
+#         n+=int(l[wt[i]][j])
+#         n+=int(dziel[wt[i][7:10]][j])
+#         t.append(n)
+    
+#     dziel[wt[i][7:10]]=t
+#     del t
+# dzielmax={}
+# for i in range(1,len(wt)):
+#    dzielmax[wt[i][7:10]]=0 
+# for i in range(1,len(wt)):
+#     nm=0
+    
+#     for j in range(12):
+#         nn=int(dziel[wt[i][7:10]][j])
+#         if nm<nn:nm=nn
+#     dzielmax[wt[i][7:10]]=nn
+# print(dzielmax)
+
+
+# # 5.4
+# mon=[]
+# alert=0
+# for j in range(12):
+#     n=0
+#     for i in range(1,len(wt)):
+#         n+=int(l[wt[i]][j])
+#     mon.append(n)
+# for i in range(12,120):
+#     n=mon[(i-12)]
+#     n=int(n*1.01)
+#     mon.append(n)
+#     if alert==0 and n>160000:
+#         print(str(i%12)+"."+str(int(2020+(i/12-i%12))))
+#         alert=1
+# plt.plot(mon)                                  
+# plt.show()  
+
+# 5.5
+mon=[]
+alert=0
+for j in range(12):
     n=0
-    for j in range(12):
+    for i in range(1,len(wt)):
         n+=int(l[wt[i]][j])
-    n+=dziel[wt[i][7:10]]
-    dziel[wt[i][7:10]]=n
-print(dziel)
+    mon.append(n)
+for i in range(12,120):
+    n=mon[(i-12)]
+    n=int(n*1.01)
+    mon.append(n)
+    if alert==0 and n>(160000+1000*(i/12-i%12)):
+        print(str(i%12)+"."+str(int(2020+(i/12-i%12))))
+        alert=1
+
