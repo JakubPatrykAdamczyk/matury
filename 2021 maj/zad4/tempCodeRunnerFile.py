5f801b17df2939889da7497c3c0abbfaa0aede73
Jakub <jakubpatrykadamczyk@gmail.com>
2022-06-27 12:06:19 +0200

    a

---
+++ 2021 maj/zad4/tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+wyraz=""
+
+for i in range(len(pol)):
+    if "DOPISZ"==pol[i]:
+        wyraz+=str(co[i])
+    if "ZMIEN"==pol[i]:
+        wyraz
+    if "USUN"==pol[i]:
+        wyraz=wyraz[0:(len(wyraz)-1)])
+    if "PRZESUN"==pol[i]:
+        wyraz
+    
+print(wyraz)
--- 2021 maj/zad4/zad4.py
+++ 2021 maj/zad4/zad4.py
@@ -1,4 +1,7 @@
+from ast import operator
 import os
+
+
 mypath=os.getcwd()
 mypath+="\\matury\\"
 mypath+="2021 maj\zad4"
@@ -41,6 +44,36 @@ for i in f:
 #         w_max=w_last
 # print("rodzaj instrukcji- "+str(w_max)+", dlugosc: "+str(n_max))
 
-# 4.3
+# # 4.3
+# l={}
+# for i in range(len(pol)):
+#     l[co[i]]=0
+# for i in range(len(pol)):
+#     if "DOPISZ"==pol[i]:
+#         n=int(l[co[i]])
+#         n+=1
+#         l[co[i]]=n
+# maxl=max(l,key= lambda x: l[x])
+# print (str(maxl)+": "+ str(l[maxl]))
 
-    
+# 4.4
+wyraz=""
+
+for i in range(len(pol)):
+    if "DOPISZ"==pol[i]:
+        wyraz+=str(co[i])
+    if "ZMIEN"==pol[i]:
+        wyraz=wyraz[0:(len(wyraz)-1)]
+        wyraz+=str(co[i])
+    if "USUN"==pol[i]:
+        wyraz=wyraz[0:(len(wyraz)-1)]
+    if "PRZESUN"==pol[i]:
+        if ord(co[i]) > (90 - 1):
+            zm = chr(ord(co[i]) + 1 - 26)
+            print(str(co[i])+": "+str(zm))
+        else:
+            zm = chr(ord(co[i]) + 1)
+            
+        wyraz.replace(co[i],zm,1)
+print(wyraz)
+print(ord("Z"))
